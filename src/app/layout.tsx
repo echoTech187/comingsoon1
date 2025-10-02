7cd37d5e601525ebfabe4d918d5005125e63218e
echotech187 <echotech187@gmail.com>
2025-10-02 16:33:38 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="robots" content="noodp"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
+        <title>Launch - Coming Soon</title>
+        <link rel="shortcut icon" href="assets/img/favicon.ico"></link>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&amp;subset=latin-ext" rel="stylesheet"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/plugins.css"></link>
+        <link rel="stylesheet" type="text/css" href="assets/css/main.css"></link>
+        </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

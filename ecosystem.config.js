deb318dff5da3a375635bcd818dc450704b15d24
echoTech187 <echotech8310@gmail.com>
2025-10-03 01:49:57 +0000

    update config

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name   : "Penerbit | Optimal Untuk Negeri",
+    script : "npm",
+    args: 'start',
+    cwd: '/var/www/optimal-publisher',
+    env: {}
+  }]
+}
